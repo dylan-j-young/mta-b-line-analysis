0f9d8a9520e583c2a32a251baa5122e94a354a8c
Dylan Young <dylan.young@colorado.edu>
2026-03-09 21:11:31 -0500

    upload project

---
+++ README.md
@@ -0,0 +1,21 @@
+# README -- PAU Analyst Skills Exercise
+
+## Overview
+
+This folder contains the code used to prepare and analyze publicly available MTA NYCT data for the PAU Analyst Skills Exercise. The analysis focuses on B Line performance trends during 2024–2025.
+
+Raw data files are not included; all inputs are sourced from the New York Open Data Portal as specified in the assignment.
+
+## Files
+
+`data_prep.py`:
+Loads raw CSV files, performs basic type checking and filtering, and stores the cleaned data in a local SQLite database for analysis.
+
+`analysis.ipynb`:
+Performs all analysis and generates figures used in the presentation, including on-time performance, service delivered, runtime lag, and runtime variability.
+
+## Execution order
+
+1. Download local copies of the datasets and store them in the `data/` folder.
+2. Run `data_prep.py` to create the SQLite database from the raw CSV inputs.
+3. Open and run `analysis.ipynb` to reproduce the analysis and figures.
---
+++ data_prep.py
@@ -0,0 +1,112 @@
+import numpy as np
+import pandas as pd
+import sqlite3
+import logging
+
+DATABASE_DIR = "data/subway_records.db"
+
+def csv_to_table_by_chunks(csv_path, table_name,
+                           date_cols=dict(), numeric_cols=[],
+                           chunksize=100000, inspect_chunk=False,
+                           replace=True):
+    """Function to convert CSV data to a SQLite table by reading in chunks (to protect RAM for large CSVs)."""
+
+    logging.info(f"Reading in: {csv_path}, and writing to table: {table_name}...")
+
+    # Force all columns to be read as strings to start
+    all_columns = pd.read_csv(csv_path, nrows=0).columns
+    dtype_dict = {col: str for col in all_columns}
+
+    # Open SQLite connection and create chunked CSV reader
+    conn = sqlite3.connect(DATABASE_DIR)
+    reader = pd.read_csv(csv_path, chunksize=chunksize, 
+                         dtype=dtype_dict,
+                         na_values=["", " ", "NULL", "NaN"])
+
+    # Iterate over chunks
+    for i, chunk in enumerate(reader):
+        # Perform initial typecasting
+        for col, fmt in date_cols.items():
+            chunk[col] = pd.to_datetime(chunk[col], format=fmt, errors="coerce")
+        for col in numeric_cols:
+            chunk[col] = pd.to_numeric(chunk[col], errors="coerce")
+
+        # Debug: don't write, just return the first chunk
+        if inspect_chunk:
+            conn.close()
+            return(chunk)
+
+        # Write to SQL
+        mode = 'replace' if (i == 0 and replace) else 'append'
+        chunk.to_sql(table_name, conn, if_exists=mode, index=False)
+        logging.info(f"Chunk {i} ingested. Total rows: {i*chunksize + len(chunk)}")
+    
+    conn.close()
+
+def setup_logging():
+    """Log to inspect data prep"""
+    log_file = "data_prep.log"
+
+    # Set up the configuration
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler(log_file),    # Writes to file
+            logging.StreamHandler()           # Prints to terminal
+        ]
+    )
+    logging.info("--- data_prep.py starting... ---")
+
+def main():
+    setup_logging()
+    
+    logging.info("Constructing SQLite database...")
+
+    # MTA Subway Trains Delayed: Beginning 2020
+    csv_to_table_by_chunks(
+        csv_path = "data/delays_all-trains.csv",
+        table_name = "delays",
+        date_cols = {"month": "%Y-%m-%d"},
+        numeric_cols = ["day_type", "delays"]
+    )
+
+    # MTA Subway End-to-End Running Times: Beginning 2019
+    csv_to_table_by_chunks(
+        csv_path = "data/running_times_all-trains.csv",
+        table_name = "running_times",
+        date_cols = {"Month": "%m/%d/%Y"},
+        numeric_cols = ["Average Actual Runtime", "25th Percentile Runtime", "50th Percentile Runtime", "75th Percentile Runtime", "Actual Trains", "Distance", "Average Speed", "Average Scheduled Runtime", "Scheduled Trains", "Number of Stops"]
+    )
+
+    # MTA Subway Schedules: 2024
+    csv_to_table_by_chunks(
+        csv_path = "data/schedule_2024_b-line.csv",
+        table_name = "schedule",
+        date_cols = {
+            "Service Date": "%m/%d/%Y",
+            "Arrival Time": "%m/%d/%Y %I:%M:%S %p",
+            "Departure Time": "%m/%d/%Y %I:%M:%S %p",
+            "Next Trip Time": "%m/%d/%Y %I:%M:%S %p"
+        },
+        numeric_cols = ["Stop Order", "Date Difference"]
+    )
+
+    # MTA Subway Schedules: 2025. Append to the same table as 2024.
+    csv_to_table_by_chunks(
+        csv_path = "data/schedule_2025_b-line.csv",
+        table_name = "schedule",
+        date_cols = {
+            "Service Date": "%m/%d/%Y",
+            "Arrival Time": "%m/%d/%Y %I:%M:%S %p",
+            "Departure Time": "%m/%d/%Y %I:%M:%S %p",
+            "Next Trip Time": "%m/%d/%Y %I:%M:%S %p"
+        },
+        numeric_cols = ["Stop Order", "Date Difference"],
+        replace=False
+    )
+
+    logging.info("--- data_prep.py completed successfully! ---")
+
+if __name__ == "__main__":
+    main()
